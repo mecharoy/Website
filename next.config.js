c46b99899ce9c43a1bf5a75d639943bf1373b19b
Abhijit Roy Choudhury <66482847+mecharoy@users.noreply.github.com>
2026-02-27 01:38:23 +0530

    fix: move serverComponentsExternalPackages out of experimental for Next.js 14+

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [],
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma', '@vercel/blob'],
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+          domains: [],
+    },
+    serverExternalPackages: ['@prisma/client', 'prisma', '@vercel/blob'],
 }
 
 module.exports = nextConfig
